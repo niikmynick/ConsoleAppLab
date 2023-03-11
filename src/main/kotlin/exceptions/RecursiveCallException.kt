afb558841ed8bdb9680c5e034599f72f8ef57530
fk724s <fk724s@mail.ru>
2023-03-11 13:19:27 +0300

    initial commit

--- src/main/kotlin/exceptions/RecursiveCallException.kt
+++ src/main/kotlin/exceptions/RecursiveCallException.kt
@@ -1,5 +1,5 @@
 package exceptions
 
 class RecursiveCallException : Exception() {
-    override val message: String = "Script can cause a recursive call so this command is skipped"
+    override val message: String = "Script can cause a recursive call so command was skipped"
 }
